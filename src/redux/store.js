435f8302c6529e07ccbbfbcada552a85405a5679
MdShimulMahmud <shimulmahmud18@gmail.com>
2023-11-05 12:18:12 +0600

    redux-counter-application

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
-
+// create store
 const store = createStore(
   rootReducer,
   composeWithDevTools(applyMiddleware(logger))
